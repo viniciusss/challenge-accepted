99c32dbcff4636fd7bcc67568a17dbc84f210b95
Vinicius de Sa Souza <vinicius@programador.eti.br>
2017-02-16 05:23:32 -0200

    Add hello world page

--- app/app.php
+++ app/app.php
@@ -5,8 +5,8 @@ $app = new Silex\Application();
 
 require_once __DIR__ . '/providers.php';
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
+$app->get('/', function () use ($app) {
+    return 'Hello world';
 });
 
 $app->run();
